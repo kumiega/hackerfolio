33cd03e02b2a93a57e4229dec4585d7662395ff7
Tomasz Kumięga <poczta@kumiega.org>
2025-10-23 21:24:45 +0200

    feat: update button variant and navigation component
    - Changed the button variant in the navigation component from "outline" to "glass" for improved visual appeal.
    - Enhanced the "glass" variant in the button component to include hover effects for better user interaction.

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,13 +5,13 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap text-md font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
+  "cursor-pointer inline-flex items-center justify-center gap-2 whitespace-nowrap text-md font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
   {
     variants: {
       variant: {
         default: "bg-primary text-primary-foreground",
         light: "bg-primary-foreground text-foreground",
-        glass: "bg-background/15 backdrop-blur-sm",
+        glass: "bg-background/15 backdrop-blur-sm hover:bg-primary hover:text-primary-foreground",
         dark: "bg-foreground text-background",
         destructive:
           "bg-destructive text-white hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
