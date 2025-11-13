895bb3d528aabd47e3ea48b18b7524bbb53c0c29
Tomasz Kumięga <poczta@kumiega.org>
2025-11-13 18:57:52 +0100

    refactor: update container structure in footer and index page
    - Modified the container elements in the footer and index page to use a maximum width of 7xl, improving layout consistency across the application.
    - Adjusted the opacity of the Callout component in the footer for better visibility and aesthetics.

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -3,11 +3,11 @@ import { Callout } from "./callout";
 const Footer = () => {
   return (
     <footer className="py-4 px-page bg-foreground text-xs">
-      <div className="container mx-auto flex flex-col gap-4 sm:text-base sm:flex-row sm:items-center sm:justify-between">
+      <div className="container mx-auto max-w-7xl flex flex-col gap-4 sm:text-base sm:flex-row sm:items-center sm:justify-between">
         <p className="text-background">
           © 2025 <span className="font-mono">Hacker</span>folio. All rights reserved.
         </p>
-        <Callout className="text-background/50 sm:text-xs">
+        <Callout className="text-background/80 sm:text-xs">
           Made with <span className="text-primary">♥</span> for devs, by devs.
         </Callout>
       </div>
